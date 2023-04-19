dd605aa684a65befef40368331a83405986697f4
Renata Inacio <revazgauska@gmail.com>
2023-04-19 13:51:05 -0300

    layoutcriar quizz

---
+++ script.js
@@ -0,0 +1,6 @@
+axios.defaults.headers.common['Authorization'] = 'BtXNJoiFoeQE4oiiOTK7wiYj';
+
+function showForm() {
+    document.querySelector(".home-screen").classList.add("hide");
+    document.querySelector(".create-quizz").classList.remove("hide");
+}
