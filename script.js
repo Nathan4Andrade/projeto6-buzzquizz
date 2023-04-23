8a2af7308fe5b4980c0bf5368e304208839671f1
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-04-23 19:35:01 -0300

    Feat: getLocalStorageQuizz

--- script.js
+++ script.js
@@ -680,3 +680,11 @@ function getMyQuizzes(listMyQuizzes) {
       oneQuizz.style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${listMyQuizzes[i].image}')`;
   }
 }
+
+
+function gettheQuizz(here) {
+  identificador = here;
+  const promise = axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/" + identificador);
+  promise.then(openQuizz);
+  promise.catch(error);
+}
